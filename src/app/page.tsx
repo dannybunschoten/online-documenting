1569c6101c1d9ab3a3aa4a843aa5fcc612ad2a84
dannybunschoten <danny.bunschoten02@gmail.com>
2025-06-26 22:03:19 +0200

    Add inspection report components and data fetching
    The commit adds a new inspection report feature with components and data
    fetching capabilities. It includes cards for displaying inspection
    details, owner information, customer data, location details and
    equipment info.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import { getData } from "@/actions";
+import InspectionReport from "./components/InspectionReport";
+
+export default async function Home() {
+  const data = await getData();
+  return <InspectionReport data={data.value[0]?.Data?.Main} />;
 }
